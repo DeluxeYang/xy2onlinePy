276d2f7a35b3e614651dcfcc1e20c340b6a17b96
DeluxeYang <421826500@qq.com>
2018-10-30 16:53:23 +0800

    2018-10-30

---
+++ lib/hand_note.py
@@ -0,0 +1,14 @@
+
+class T:
+    def __init__(self, a):
+        self.__setattr__("on_a", a)
+
+    def handle_event(self, name):
+        return hasattr(self, "on_"+name)
+
+    def on_q(self):
+        return "123"
+
+t = T(1)
+print()
+print(t.handle_event("a"))
