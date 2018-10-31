27c48130020d9ce2162d02a10c304990a60de6fa
DeluxeYang <421826500@qq.com>
2018-10-31 16:06:44 +0800

    state

---
+++ lib/state/state.py
@@ -0,0 +1,12 @@
+class State:
+    def __init__(self):
+        pass
+
+    def update(self, dt):
+        pass
+
+    def draw(self, screen):
+        pass
+
+    def late_draw(self, screen):
+        pass
