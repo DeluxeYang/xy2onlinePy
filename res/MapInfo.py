f6603271f7628a079ad529a383be66c0c652dc0d
DeluxeYang <421826500@qq.com>
2018-11-13 17:00:01 +0800

    2018-11-13

--- res/MapInfo.py
+++ res/MapInfo.py
@@ -17,7 +17,7 @@ map_info = {
         "name": "化生寺",
         "portal": [
             {"position": (3721, 2754), "target_map_id": "newscene/1410.map", "target_position": (190, 390),
-             "show": True, "wdf": "mapani.wdf", "hash": "0xC0570C07"},
+             "show": True, "wdf": "mapani.wdf", "_hash": "0xC0570C07"},
         ],
         "NPC": []
     },
@@ -27,7 +27,7 @@ map_info = {
         "name": "长安城",
         "portal": [
             {"position": (100, 310), "target_map_id": "scene/1002.map", "target_position": (3625, 2685),
-             "show": True, "wdf": "mapani.wdf", "hash": "0xC0570C07"},
+             "show": True, "wdf": "mapani.wdf", "_hash": "0xC0570C07"},
         ],
         "NPC": {}
     },
