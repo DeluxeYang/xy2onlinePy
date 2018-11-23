25923f4aac5f6559a6496445e52be4ac57e9758b
DeluxeYang <421826500@qq.com>
2018-11-23 14:51:50 +0800

    textbox

--- core/ui/textbox/textbox.py
+++ core/ui/textbox/textbox.py
@@ -1,8 +1,9 @@
 from core.entity.ui import UI
+from utils import ptext
 
 
 class TextBox(UI):
-    def __init__(self, parent, target, x, y, w, h):
+    def __init__(self, parent, target, x=0, y=0, w=0, h=0):
         super().__init__(x, y, w, h)
         self.target = target
         self.text = str(target)
