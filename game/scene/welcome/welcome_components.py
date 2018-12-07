2fd3989edf7deeaa60572fb5670343b47935f4fd
DeluxeYang <421826500@qq.com>
2018-12-07 18:33:21 +0800

    打开链接组件

--- game/scene/welcome/welcome_components.py
+++ game/scene/welcome/welcome_components.py
@@ -30,10 +30,9 @@ class WelcomeLinkedButtonMouseComponent(UIMouseComponent):
 
     def on_mouse_left_up(self, event):
         if self.is_mouse_in_rect(event):
-            def callback(param):  # Button按键事件回调
-                print(param)
-            self.game_object.callback = callback
-            self.game_object.param = "打开链接"
+            import webbrowser
+            webbrowser.open("http://xy2.163.com")
+            print("???")
             event.handled = True
             self.game_object.focus = True  # 如果鼠标仍在范围之内
         self.game_object.is_mouse_up = True
