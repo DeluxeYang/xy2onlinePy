670337a21f68b5f78a6e417f86e03fbf6615f3f1
DeluxeYang <421826500@qq.com>
2018-12-18 15:27:09 +0800

    表情

---
+++ core/ui/textfield/emoji.py
@@ -0,0 +1,13 @@
+from core.ui.ui import UI
+from .emoji_state import EmojiState
+
+
+class Emoji(UI):
+    def __init__(self, res_info, x=0, y=0, w=0, h=0):
+        super().__init__(res_info, x, y, w, h)
+        self.init_state(EmojiState())
+
+
+def emoji_factory(res_tuple, x, y):
+    res_info = {"normal": res_tuple}
+    return Emoji(res_info, x, y)
---
+++ core/ui/textfield/emoji_state.py
@@ -0,0 +1,5 @@
+from core.ui.state.animated_ui_state import AnimatedUIState
+
+
+class EmojiState(AnimatedUIState):
+    res_index = "normal"
