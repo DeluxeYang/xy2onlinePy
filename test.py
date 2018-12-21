d43deed7fe2e9c2852e0b6abe04f6735edee2622
DeluxeYang <421826500@qq.com>
2018-12-21 17:11:24 +0800

    2018-12-21

--- test.py
+++ test.py
@@ -1,27 +1,68 @@
-"""Quick usage examples from the README file"""
+templates = {
+    "#red": ["color", "red"],
+    "#24": ["gires2.wdf", "0x12345678"],
+    "#35": ["gires2.wdf", "0x12345678"],
+    "#2": ["gires2.wdf", "0x12345678"]
+}
 
-import pygame
-from utils import ptext
+prefix = {
+    "#": 1,
+    "#r": 1, "#re": 1,
+    "#2": 1,
+    "#3": 1
+}
 
-from settings import XY2PATH
+text = "啊#35"
 
-screen = pygame.display.set_mode((854, 480))
-screen.fill((0, 30, 60))
+def t():
 
+    contents = []
+    i = 0
+    pattern_matching = False
+    last_pattern = None  # color, bold, italic
+    pattern_text_cache = "#"
+    while i < len(text):
+        if text[i] == "#":  # 如果是#
+            pattern_matching = True
+        elif text[i].isalnum():  # 如果是数字或者字母
+            if pattern_matching:  # 如果此时正在匹配
+                pattern_text_cache += text[i]  # 则暂存该字符
+                if pattern_text_cache in templates:  # 如果有该模式
+                    last_pattern = templates[pattern_text_cache]  # 记录
+                if pattern_text_cache not in prefix:  # 如果前缀不匹配
+                    pattern_matching = False  # 退出匹配模式
+                    if last_pattern:
+                        contents.append(pattern_transform(last_pattern))
+                    else:
+                        for x in pattern_text_cache:
+                            contents.append(x)
+                    last_pattern = None
+                    pattern_text_cache = "#"
+            else:
+                contents.append(text[i])
+        else:
+            if pattern_matching:
+                pattern_matching = False  # 退出匹配模式
+                if last_pattern:
+                    contents.append(pattern_transform(last_pattern))
+                else:
+                    for x in pattern_text_cache:
+                        contents.append(x)
+                last_pattern = None
+                pattern_text_cache = "#"
+            contents.append(text[i])
+        i += 1
+    if last_pattern:
+        contents.append(pattern_transform(last_pattern))
+    elif pattern_text_cache != "#":
+        for x in pattern_text_cache:
+            contents.append(x)
+    return contents
 
-# ptext.draw("Font name and size", (20, 100), fontname="fonts/Boogaloo.ttf", fontsize=60)
-# ptext.draw("Font decoration", (300, 180), sysfontname="freesans", italic=True, underline=True)
-# ptext.draw("Positioned text", topright=(840, 20))
-# ptext.draw("Allow me to demonstrate wrapped text.", (90, 210), width=180, lineheight=1.5)
-# ptext.draw("Outlined text", (400, 70), owidth=1.5, ocolor=(255,255,0), color=(0,0,0))
-# ptext.draw("Drop shadow", (640, 110), shadow=(2,2), scolor="#202020")
-# ptext.draw("Color gradient", (540, 170), color="red", gcolor="purple")
-# ptext.draw("Transparency", (700, 240), alpha=0.1)
-# ptext.draw("Vertical text", midleft=(40, 440), angle=90)
-ptext.draw("All together now:\n大话西游",
-    midbottom=(427,460), width=360, fontname="hyi1gjm", fontsize=48, underline=True,
-    color="#AAFF00", gcolor="#66AA00", owidth=1.5, ocolor="black", alpha=0.8, angle=5, align="left")
+def pattern_transform(pattern):
+    if pattern[0] == "color":
+        return "Color"
+    else:
+        return "Emoji"
 
-pygame.display.flip()
-while not any(event.type in (pygame.KEYDOWN, pygame.QUIT) for event in pygame.event.get()):
-	pass
+print(t())
