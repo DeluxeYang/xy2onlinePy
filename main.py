52ed64dd4db3bb0464cf90d5459b526ec1ca1a82
DeluxeYang <421826500@qq.com>
2018-12-25 14:07:51 +0800

    2018-12-25

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from core.world.director import director
 
 from game.scene.welcome.welcome_scene import WelcomeScene
 
-scene = WelcomeScene()
+from game.scene.notice.notice_scene import NoticeScene
+
+scene = NoticeScene()
 
 director.run(scene)
