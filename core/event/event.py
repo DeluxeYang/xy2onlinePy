4a001e89af14b03c03835004d8a18e4c04f757b4
DeluxeYang <421826500@qq.com>
2019-01-02 12:26:24 +0800

    Event

--- core/event/event.py
+++ core/event/event.py
@@ -53,18 +53,20 @@ def event_filter():
 
 
 my_events = {
-    12: "quit",
-    5: {
+    QUIT: "quit",
+    MOUSEBUTTONDOWN: {
         1: "mouse_left_down",  # MOUSE BUTTON DOWN = 5
         2: "mouse_mid_down",
         3: "mouse_right_down",
         4: "mouse_wheel_forward_down",
         5: "mouse_wheel_backward_down"
     },
-    6: {
+    MOUSEBUTTONUP: {
         1: "mouse_left_up",  # MOUSE BUTTON UP = 6
         2: "mouse_mid_up",
         3: "mouse_right_up",
     },
-    4: "mouse_motion"
+    MOUSEMOTION: "mouse_motion",
+    KEYDOWN: "key_down",
+    KEYUP: "key_up"
 }
