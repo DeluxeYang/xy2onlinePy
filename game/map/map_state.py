c9722bcc44f8ed1723ef60f3477129db9d010d57
DeluxeYang <421826500@qq.com>
2019-01-14 13:53:45 +0800

    2019-1-14

--- game/map/map_state.py
+++ game/map/map_state.py
@@ -22,11 +22,15 @@ class MapState(State):
         masks = []
         units = quest_16(self.game_object.window, self.game_object.row, self.game_object.col,
                          width_margin=60, height_margin=40)
+        flag = True
         for i in units:
+            if not self.game_object.unit_has_blitted[i]:
+                flag = False
             for mask in self.game_object.masks_of_unit[i]:
                 if (mask.rect.x, mask.rect.y) not in no_repeat:
                     no_repeat[(mask.rect.x, mask.rect.y)] = True
                     masks.append(mask)
+        self.game_object.ready = flag
         context["other_masks"] = masks
         context["left_top"] = self.game_object.get_left_top()
         context["collision_window"] = self.game_object.get_collision_window()
