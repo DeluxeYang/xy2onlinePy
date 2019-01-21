d94e1a56c3a0447f4ede2430f7bc780a93e69bfe
DeluxeYang <421826500@qq.com>
2019-01-21 13:59:39 +0800

    account select scene

--- game/scene/account_select/account_select_scene.py
+++ game/scene/account_select/account_select_scene.py
@@ -1,1 +1,57 @@
 from core.world.scene import Scene
+from game.scene.account_select.component import \
+    EnterButtonMouseComponent, CancelButtonMouseComponent
+
+
+class AccountSelectScene(Scene):
+    scene_init_data = {
+        "id": "3",
+        "name": "account_select_scene",
+        "title": "大话西游II",
+        "resolution": (640, 480),
+        "layers": {
+            "ui": [
+                {
+                    "type": "fixed",
+                    "screen_position": (305, 250),
+                    "w": 220,
+                    "h": 50,
+                    "res_info": None,
+                    "factor": [
+                        {
+                            "type": "button",
+                            "attributes": {
+                                "x": 5,
+                                "y": 5,
+                                "w": 91,
+                                "h": 37,
+                                "res_info": {"normal": ["gires2.wdf", "0xC3E7E556"]}
+                            },
+                            "components": [EnterButtonMouseComponent()],
+                        },
+                        {
+                            "type": "button",
+                            "attributes": {
+                                "x": 125,
+                                "y": 5,
+                                "w": 91,
+                                "h": 37,
+                                "res_info": {"normal": ["gires2.wdf", "0xCB250B45"]}
+                            },
+                            "components": [CancelButtonMouseComponent()],
+                        },
+                    ]
+                },
+            ],
+            "shape": [
+
+            ],
+            "map": [
+                {
+                    "type": "static",
+                    "world_position": (0, 0),
+                    "res_info": {"normal": ["gires2.wdf", "0x51F941C4"]}
+                },
+            ]
+        }
+    }
---
+++ game/scene/account_select/component.py
@@ -0,0 +1,45 @@
+from core.ui.ui_mouse_component import UIMouseComponent
+from core.event.event import post_event
+
+
+
+
+class EnterButtonMouseComponent(UIMouseComponent):
+    def on_mouse_over(self, event):
+        if self.is_mouse_in_rect(event):
+            pass
+
+    def on_mouse_left_up(self, event):
+        if self.is_mouse_in_rect(event):
+            # self.game_object.callback = post_event
+            # self.game_object.param = {"name": "change_scene",
+            #                           "scene": AccountSelectScene}
+            event.handled = True
+            self.game_object.focus = True  # 如果鼠标仍在范围之内
+        self.game_object.is_mouse_up = True
+
+    def on_mouse_left_down(self, event):
+        if self.is_mouse_in_rect(event):
+            self.game_object.is_mouse_down = True
+            event.handled = True
+
+
+class CancelButtonMouseComponent(UIMouseComponent):
+    def on_mouse_over(self, event):
+        if self.is_mouse_in_rect(event):
+            pass
+
+    def on_mouse_left_up(self, event):
+        if self.is_mouse_in_rect(event):
+            from game.scene.welcome.welcome_scene import WelcomeScene
+            self.game_object.callback = post_event
+            self.game_object.param = {"name": "change_scene",
+                                      "scene": WelcomeScene}
+            event.handled = True
+            self.game_object.focus = True  # 如果鼠标仍在范围之内
+        self.game_object.is_mouse_up = True
+
+    def on_mouse_left_down(self, event):
+        if self.is_mouse_in_rect(event):
+            self.game_object.is_mouse_down = True
+            event.handled = True
