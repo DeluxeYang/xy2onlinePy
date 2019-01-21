b0f6fa6eefd035009cbf34d156e35cdfe1050b44
DeluxeYang <421826500@qq.com>
2019-01-22 00:01:25 +0800

    2019-1-22

--- core/ui/text_button/text_button_base_component.py
+++ core/ui/text_button/text_button_base_component.py
@@ -3,7 +3,7 @@ from core.ui.ui_mouse_component import UIMouseComponent
 
 class TextButtonBaseComponent(UIMouseComponent):
     def on_mouse_over(self, event):
-        if self.is_mouse_in_rect(event):
+        if self.is_mouse_in_rect(event, False):
             self.game_object.is_mouse_over = True
         else:
             self.game_object.is_mouse_over = False
--- core/ui/ui_mouse_component.py
+++ core/ui/ui_mouse_component.py
@@ -3,8 +3,8 @@ from core.event.event import post_event
 
 
 class UIMouseComponent(UIComponent):
-    def is_mouse_in_rect(self, event):
+    def is_mouse_in_rect(self, event, reset_focus=True):
         flag = self.game_object.screen_rect.collidepoint(event.pos[0], event.pos[1])  # 且鼠标在ui的范围内
-        if flag:
+        if flag and reset_focus:
             post_event({"name": "reset_ui_focus", "set_focus_obj": self.game_object})
         return flag
