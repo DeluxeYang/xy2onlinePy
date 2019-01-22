97e36dd1304ef3236ed0cb982e4a7a33ec68a0f2
DeluxeYang <421826500@qq.com>
2019-01-22 13:47:12 +0800

    2019-1-22

--- core/ui/button/button.py
+++ core/ui/button/button.py
@@ -3,11 +3,8 @@ from core.ui.button.button_state import ButtonNormalState
 
 
 class Button(UI):
-    def __init__(self, res_info, parent=None, text=None, x=0, y=0, w=0, h=0):
+    def __init__(self, res_info, x=0, y=0, w=0, h=0):
         super().__init__(res_info, x, y, w, h)
-        self.text = str(text)
-
-        self.parent = parent
 
         self.is_mouse_down = False
         self.is_mouse_up = False
