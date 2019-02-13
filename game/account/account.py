18078c7bbf52ad7c0ac73915f9ea57cb93bdec94
DeluxeYang <421826500@qq.com>
2019-02-13 22:47:20 +0800

    2019-2-13

--- game/account/account.py
+++ game/account/account.py
@@ -3,3 +3,9 @@ class Account:
         self.account = account
         self.role_num = 0
         self.roles = []
+
+    def accept_roles(self, roles):
+        self.roles += roles
+
+    def add_role(self):
+        pass
