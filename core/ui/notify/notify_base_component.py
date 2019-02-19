88bf3e07c250e909e91c830629d89647eae56554
DeluxeYang <421826500@qq.com>
2019-02-19 23:54:44 +0800

    notify的销毁由mouse_up触发

--- core/ui/notify/notify_base_component.py
+++ core/ui/notify/notify_base_component.py
@@ -4,6 +4,9 @@ from core.ui.ui_mouse_component import UIMouseComponent
 class NotifyMouseComponent(UIMouseComponent):
     def on_mouse_left_down(self, event):
         if self.is_mouse_in_rect(event):
-            print(self.game_object.z)
+            event.handled = True
+
+    def on_mouse_left_up(self, event):
+        if self.is_mouse_in_rect(event):
             self.game_object.destroy()
             event.handled = True
