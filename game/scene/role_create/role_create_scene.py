a228adade7be139a32fb4bbd6f5c021d28bda34c
DeluxeYang <421826500@qq.com>
2019-02-20 23:15:52 +0800

    角色创建场景

--- game/scene/role_create/role_create_scene.py
+++ game/scene/role_create/role_create_scene.py
@@ -1,1 +1,23 @@
 from core.world.scene import Scene
+
+
+class RoleCreateScene(Scene):
+    scene_init_data = {
+        'id': '4',
+        'name': 'role_select_scene',
+        "title": "大话西游II",
+        "resolution": (640, 480),
+        "layers": {
+            "ui": [
+
+            ],
+            "shape": [],
+            "map": [
+                {
+                    "type": "static",
+                    "world_position": (0, 0),
+                    "res_info": {"normal": ["gires2.wdf", "0x4B291E02"]}
+                },
+            ]
+        }
+    }
