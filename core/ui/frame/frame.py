0b89ba6e41948e151e8d0dcd9a28fdc4781d2386
DeluxeYang <421826500@qq.com>
2019-02-26 23:10:18 +0800

    动画框

--- core/ui/frame/frame.py
+++ core/ui/frame/frame.py
@@ -1,5 +1,5 @@
 from core.ui.ui import UI
-from core.ui.frame.frame_state import FrameState
+from core.ui.frame.frame_state import FrameState, AnimatedFrameState
 
 
 class FixedFrame(UI):
@@ -11,6 +11,12 @@ class FixedFrame(UI):
         super().add_child(child)
         if hasattr(child, "ui_id"):
             self.__setattr__(child.ui_id, child)
+
+
+class AnimatedFrame(UI):
+    def __init__(self, res_info, x=0, y=0, w=0, h=0, ui_id=""):
+        super().__init__(res_info, x, y, w, h, ui_id)
+        self.init_state(AnimatedFrameState())
 
 
 class MovableFrame(FixedFrame):
--- core/ui/frame/frame_state.py
+++ core/ui/frame/frame_state.py
@@ -1,5 +1,21 @@
 from core.ui.state.static_ui_state import StaticUIState
+from core.ui.state.animated_ui_state import AnimatedUIState
+from settings import ResMargin, logger
 
 
 class FrameState(StaticUIState):
     res_index = "normal"
+
+
+class AnimatedFrameState(AnimatedUIState):
+    res_index = "normal"
+
+    def update(self, context):
+        if self.game_object.res_info:
+            super().update(context)
+
+    def draw(self, screen):
+        w, h = self.game_object.surface.get_size()
+        temp_rect = self.game_object.screen_rect.move(
+            -((w-self.game_object.w)//2), -((h-self.game_object.h)//2))
+        screen.blit(self.game_object.surface, temp_rect)
