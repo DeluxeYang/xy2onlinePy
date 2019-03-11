dbd8b47e26ffc60d4b5ce7bb0cf273f944865fc5
DeluxeYang <421826500@qq.com>
2019-03-11 23:53:24 +0800

    Frame的Store

--- core/ui/frame/frame.py
+++ core/ui/frame/frame.py
@@ -3,8 +3,10 @@ from core.ui.frame.frame_state import FrameState, AnimatedFrameState
 
 
 class FixedFrame(UI):
-    def __init__(self, res_info, x=0, y=0, w=0, h=0, ui_id=""):
+    def __init__(self, res_info, x=0, y=0, w=0, h=0, ui_id="", **kwargs):
         super().__init__(res_info, x, y, w, h, ui_id)
+        for k, v in kwargs.items():
+            self.__setattr__(k, v)
         self.init_state(FrameState())
 
     def add_child(self, child):
