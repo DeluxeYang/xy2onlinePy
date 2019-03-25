9741659b7a8450f451525eceaadb65346144ffc1
DeluxeYang <421826500@qq.com>
2019-03-25 22:50:25 +0800

    2019-3-25

--- core/account/account.py
+++ core/account/account.py
@@ -8,16 +8,6 @@ class Account:
         self.current_role = None
         self.current_role_name = ''
 
-    @property
-    def current_role(self):
-        self.current_role = self.roles[self.current_role_name]
-        return self.current_role
-
-    @current_role.setter
-    def current_role(self, role_name):
-        self.current_role_name = role_name
-        self.current_role = self.roles[self.current_role_name]
-
     def add_role(self, role):
         self.roles[role.name] = role
 
