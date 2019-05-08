46ce9a1a2c3b73835c492ef06dd39501c95d57bd
DeluxeYang <421826500@qq.com>
2019-05-08 14:32:55 +0800

    人物移动信息向服务端发送，其他玩家可接收该移动信息，并更新shape

--- game/character/character_component.py
+++ game/character/character_component.py
@@ -9,6 +9,14 @@ class MainCharacterComponent(Component):
         self.state.game_object.target_list = event.path_list
         self.state.game_object.is_new_target = True
         self.state.game_object.is_running = event.is_running
+        director.network_client.request(send_data={  # 获取当前主要角色所在场景中的其他玩家
+            "action": "moving",
+            "account": director.account.account,
+            "role_name": director.account.get_main_role().name,
+            "map_id": director.account.get_main_role().map_id,
+            "path_list": event.path_list,
+            "is_running": event.is_running
+        })
         event.handled = True
 
 
@@ -37,6 +45,13 @@ class CharacterMouseComponent(Component):
             if self.state.game_object.mask.mask.overlap(event.mouse_point_mask.mask, (offset_x, offset_y)):
                 return True
         return False
+
+    def on_receive_moving(self, event):
+        if event.map_id == director.account.get_main_role().map_id and self.state.game_object.name == event.role_name:
+            self.state.game_object.target_list = event.path_list
+            self.state.game_object.is_new_target = True
+            self.state.game_object.is_running = event.is_running
+            event.handled = True
 
     # def draw(self, screen=None):
     #     pygame.draw.rect(screen, (0, 0, 0), self.state.game_object.screen_rect)
